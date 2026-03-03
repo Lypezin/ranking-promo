559cbe9434484a7cf7a1da97e4884ea33266c73a
lypezin <foolype@gmail.com>
2026-03-03 11:28:58 -0300

    chore: configuration setup

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // ==========================================
 
 // 1. Substitua pela URL da sua API (Project Settings -> API -> URL)
-const SUPABASE_URL = 'COLE_SUDO_SUPABASE_URL_AQUI';
+const SUPABASE_URL = 'https://adwtbqdrdwdxjqjhbict.supabase.co';
 
 // 2. Substitua pela sua chave anônima (Project Settings -> API -> anon public)
-const SUPABASE_ANON_KEY = 'COLE_SUDO_SUPABASE_ANON_KEY_AQUI';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkd3RicWRyZHdkeGpxamhiaWN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NDQ5MDIsImV4cCI6MjA4ODEyMDkwMn0.o4EkbsBOnXeJY1xv4rKqh_c52R3DYpiDIcNi8BIIPcQ';
